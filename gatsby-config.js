24d9548665f08a0a3faac0e8140fbbaefaffa575
vaish725 <vaishnaviskamdi@gmail.com>
2025-09-13 22:16:52 -0400

    Remove PNG logo files and disable icon reference - use SVG components with V

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,7 @@ module.exports = {
         background_color: `#0a192f`,
         theme_color: `#0a192f`,
         display: `minimal-ui`,
-        icon: `src/images/logo.png`,
+        // icon: `src/images/logo.png`, // Temporarily disabled
       },
     },
   ],
