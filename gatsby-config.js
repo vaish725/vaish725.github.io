390ac6c4525112fdba9547222029660dfb2ff763
vaish725 <vaishnaviskamdi@gmail.com>
2025-09-13 21:35:28 -0400

    Simplify gatsby-config and upgrade to Node.js 20

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,39 +16,16 @@ module.exports = {
         path: `${__dirname}/content/`,
       },
     },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-external-links`,
-            options: {
-              target: `_blank`,
-              rel: `nofollow noopener noreferrer`,
-            },
-          },
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 700,
-              linkImagesToOriginal: true,
-              quality: 90,
-            },
-          },
-        ],
-      },
-    },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: config.siteTitle,
         short_name: config.siteTitle,
         start_url: `/`,
-        background_color: config.colors.darkNavy,
-        theme_color: config.colors.navy,
+        background_color: `#0a192f`,
+        theme_color: `#0a192f`,
         display: `minimal-ui`,
         icon: `src/images/logo.png`,
       },
